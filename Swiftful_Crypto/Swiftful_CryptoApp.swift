a30fbb8909a096e3b039d589f871707b537b6a06
Jung Hwan Park <hwann1005@gmail.com>
2023-02-07 20:48:44 +0900

    Initial Commit

---
+++ Swiftful_Crypto/Swiftful_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftful_CryptoApp.swift
+//  Swiftful_Crypto
+//
+//  Created by Jung Hwan Park on 2023/02/07.
+//
+
+import SwiftUI
+
+@main
+struct Swiftful_CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
